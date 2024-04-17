711d224931269878ed270913748bbce6ac4b64d8
JayJay1024 <lygm1062@gmail.com>
2024-04-17 10:31:54 +0800

    Token tooltip short address

--- packages/xtoken-ui/src/components/transfer-chain-section.tsx
+++ packages/xtoken-ui/src/components/transfer-chain-section.tsx
@@ -5,7 +5,7 @@ import TransferSwitch from "./transfer-switch";
 import ComponentLoading from "@/ui/component-loading";
 import { Address } from "viem";
 import Image from "next/image";
-import { getTokenLogoSrc } from "@/utils";
+import { getTokenLogoSrc, toShortAdrress } from "@/utils";
 import CopyIcon from "@/ui/copy-icon";
 
 interface Recipient {
@@ -121,7 +121,7 @@ function TokenTips({ token, chain }: { token: Token; chain: ChainConfig }) {
             target="_blank"
             href={explorer.href}
           >
-            {token.address}
+            {toShortAdrress(token.address, 12, 10)}
           </a>
           <CopyIcon text={token.address} copiedColor="#ffffff" />
         </div>
