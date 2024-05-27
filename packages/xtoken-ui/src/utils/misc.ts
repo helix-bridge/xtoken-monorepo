975af251ec9de90420c28b485f02247aa2412908
JayJay1024 <lygm1062@gmail.com>
2024-05-27 10:03:39 +0800

    Fix msgport api

--- packages/xtoken-ui/src/utils/misc.ts
+++ packages/xtoken-ui/src/utils/misc.ts
@@ -40,8 +40,8 @@ export async function fetchMsglineFeeAndParams(
   sender: Address,
   payload: Hex,
 ) {
-  const endpoint = "https://api.msgport.xyz/ormp/fee";
-  // const endpoint = 'https://msgport-api.darwinia.network/ormp/fee'
+  // const endpoint = "https://api.msgport.xyz/ormp/fee"; // v2
+  const endpoint = "https://msgport-api.darwinia.network/ormp/fee"; // v1
   // const endpoint = "http://g2.generic.darwinia.network:3378/ormp/fee";
   const feeData = await fetch(
     `${endpoint}?from_chain_id=${fromChainId}&to_chain_id=${toChainId}&payload=${payload}&from_address=${fromMessager}&to_address=${toMessager}&refund_address=${sender}`,
